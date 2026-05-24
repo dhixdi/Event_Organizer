9aa11ff9425635bfb61f579a35e55f3e7618e438
rizki <ardhikaz30@gmail.com>
2026-05-25 00:05:20 +0700

    change docker

--- src/config/database.js
+++ src/config/database.js
@@ -10,7 +10,18 @@ module.exports = {
     dialect: process.env.DB_DIALECT || 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: process.env.DB_DIALECT || 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Wajib untuk Cloud SQL
+      },
+    },
+    logging: false, // Matikan log SQL di production
   },
 };
